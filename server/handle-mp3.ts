729a038fa6e41c5a138f9935cfd028ae80e21b16
Matheus Motta <matheusdomingues423@gmail.com>
2023-08-16 15:49:25 -0300

    ffmpeg handler

---
+++ server/handle-mp3.ts
@@ -0,0 +1,19 @@
+import ffmpegStatic from 'ffmpeg-static'
+import ffmpeg from 'fluent-ffmpeg'
+
+export const handleMP3 = () => new Promise<void>((resolve, reject) => {
+  ffmpeg.setFfmpegPath(ffmpegStatic as string)
+
+  ffmpeg()
+    .input('video.mp4')
+    .outputOptions('-ab', '192k')
+    .saveToFile('audio.mp3')
+    .on('end', () => {
+      console.log('[CONVERSION_FINISHED]')
+      resolve()
+    })
+    .on('error', (error) => {
+      console.log(error)
+      reject()
+    })
+})
