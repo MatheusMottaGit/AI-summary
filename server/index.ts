147edcdc056f596cf58d9a285ca4985e3dda304f
Matheus Motta <matheusdomingues423@gmail.com>
2023-08-24 15:17:25 -0300

    running-server

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import cors from "@fastify/cors"
 import { downloader } from "./download"
 import { z } from "zod"
 import { handleMP3 } from "./handle-mp3"
+import { handleTranscription } from "./transcription"
 
 const app = fastify()
 
@@ -15,12 +16,26 @@ app.get('/audio', async (request, response) => {
     videoID: z.string()
   })
 
-  const { videoID } = paramsSchema.parse(request.params)
+  const { videoID } = paramsSchema.parse(request.query)
 
   try {
     await downloader(videoID)
 
     await handleMP3()
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/role', async (request) => {
+  const bodySchema = z.object({
+    message: z.string()
+  })
+
+  const { message } = bodySchema.parse(request.body)
+
+  try {
+    await handleTranscription(message)
   } catch (error) {
     console.log(error)
   }
