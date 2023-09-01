1e5f99774fb5fe9c91414600b590ea516223793f
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-01 14:36:52 -0300

    app-main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,86 +1,10 @@
 'use client'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { FormEvent, useState } from 'react'
-import { formatVideoURL } from '@/utils/urlFormater'
-import axios from 'axios'
-import { Textarea } from '@/components/ui/textarea'
-
-interface Transcription {
-  transcription: {
-    text: string
-  }
-}
 
 export default function Home() {
 
-  const [videoUrl, setVideoUrl] = useState('')
-  // const [fileContent, setFileContent] = useState<Transcription | string>('')
-
-  async function handleSendURL(event: FormEvent<HTMLFormElement>) {
-    try {
-      event.preventDefault()
-
-      const formData = new FormData(event.currentTarget)
-      const inputURL = String(formData.get('url'))
-
-      const { embedURL, videoID } = formatVideoURL(inputURL)
-      setVideoUrl(embedURL)
-
-      await axios.get(`http://localhost:3333/audio`, {
-        params: {
-          videoID: videoID
-        }
-      })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async function getVideoTranscription(event: FormEvent<HTMLFormElement>) {
-    try {
-      event.preventDefault()
-
-      const formData = new FormData(event.currentTarget)
-      const textarea = String(formData.get('textarea'))
-
-      console.log(textarea)
-
-      const response = await axios.post('http://localhost:3333/role', { textarea })
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
   return (
-    <main className={`w-full p-4 flex flex-col items-center justify-center ${videoUrl ? 'gap-5' : 'gap-12'}`}>
-      <div className='flex flex-col text-center'>
-        <h1 className='font-bold text-xl'>File generator</h1>
+    <>
 
-        <span className='text-zinc-300 text-sm'>Using AI for creating a videos summary to help you</span>
-      </div>
-
-      <form onSubmit={handleSendURL} className='flex items-center gap-2 w-[548px]'>
-        <Input required placeholder='Insert a youtube video URL...' name='url' />
-
-        <Button type='submit'>Send</Button>
-      </form>
-
-      <div className='w-[548px] flex flex-col gap-4 border border-zinc-200 rounded-md p-4'>
-        {
-          videoUrl ? (
-            <iframe className='rounded-md h-64' src={videoUrl}></iframe>
-          ) : (
-            <></>
-          )
-        }
-
-        <form className='flex flex-col gap-2' onSubmit={getVideoTranscription}>
-          <Textarea name='textarea' placeholder='Give AI a context about your video to get a better experience...' />
-
-          <Button type='submit'>Generate File</Button>
-        </form>
-      </div>
-    </main>
+    </>
   )
 }
