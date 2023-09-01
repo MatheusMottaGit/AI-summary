6e3f4903cd430c4917d59c287a6f745fe9618f60
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-01 14:35:12 -0300

    handling-video-transcription

--- server/transcription.ts
+++ server/transcription.ts
@@ -1,29 +1,19 @@
 import fs from "fs";
-import OpenAI from 'openai'
+import { openai } from "./lib/openai";
 
-const openai = new OpenAI({ apiKey: '' });
-
-export async function handleTranscription(message: string) {
-  await openai.audio.transcriptions.create({
-    file: fs.createReadStream("audio.mp3"),
+export async function handleTranscription() {
+  openai.audio.transcriptions.create({
+    file: fs.createReadStream("./server/audio.mp3"),
     model: "whisper-1",
   })
     .then(response => {
       console.log('[TRANSCRIPTION_STARTED]')
-      fs.writeFileSync('output.txt', response.text, 'utf-8')
+
+      fs.writeFileSync('./server/output.txt', response.text, 'utf-8')
+
+      console.log('[FINISHED_TRANSCRIPTION]')
     })
     .catch(error => {
       console.log(error)
-    })
-
-  await openai.chat.completions.create({
-    model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'user', content: `${message}` },
-      { role: 'user', content: fs.readFileSync('output.txt', 'utf-8') }
-    ]
-  })
-    .then(response => {
-      fs.writeFileSync('summary.txt', response.choices[0].message.content as string)
     })
 }
