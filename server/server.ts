06c866f8c14b769f779cb2f1916115f9683662e5
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-01 14:33:43 -0300

    server-route

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import jwt from '@fastify/jwt'
+import { audioRoutes } from "./routes/audioRoutes"
+import { transcriptionRoutes } from "./routes/transcriptionRoutes"
+import { authRoutes } from "./routes/auth"
+
+const app = fastify()
+
+app.register(cors, {
+  origin: true
+})
+
+app.register(jwt, {
+  secret: 'huivefhwkcohuriohu4ihgurigquguihfjklw'
+})
+
+app.register(audioRoutes)
+app.register(transcriptionRoutes)
+app.register(authRoutes)
+
+app
+  .listen({ port: 3333 })
+  .then(() => console.log("server running"))
+
