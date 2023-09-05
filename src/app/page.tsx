2baf4722a97ce85fbe9cc3c8b10310262c50991b
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-05 09:25:26 -0300

    main-app-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-'use client'
-
 export default function Home() {
 
   return (
-    <>
-
-    </>
+    <div className="w-full">
+      <div className="flex items-center justify-between">
+        <h1 className="font-bold text-zinc-50 text-xl">Dashboard</h1>
+      </div>
+    </div>
   )
 }
