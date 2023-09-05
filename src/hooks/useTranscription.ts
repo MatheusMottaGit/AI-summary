7c22b2b439eca049a18b9c31f38ddf36db94da42
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-05 09:27:06 -0300

    building-shadcn/ui-components

--- src/hooks/useTranscription.ts
+++
@@ -1,7 +0,0 @@
-import { TranscriptionContext } from "@/app/contexts/Transcription"
-import { useContext } from "react"
-
-export const useTranscription = () => {
-  const context = useContext(TranscriptionContext)
-  return context
-}
--- src/types/transcription.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch, FormEvent, SetStateAction } from "react"
-
-export interface Transcription {
-  videoUrl: string
-  handleSendURL: (event: FormEvent<HTMLFormElement>) => Promise<void>
-  getVideoTranscription: (event: FormEvent<HTMLFormElement>) => Promise<void>
-}
