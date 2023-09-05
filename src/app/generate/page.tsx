d38425eea2659cf93c5cd767471aa2e29178d07a
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-05 16:16:23 -0300

    generate-page

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,17 +1,36 @@
-import React from 'react'
+'use client'
+import React, { FormEvent, useState } from 'react'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { CheckCheckIcon } from 'lucide-react'
 import EmptyVideo from '@/components/generate/EmptyVideo'
 import PromptForm from '@/components/generate/PromptForm'
 import TitleAndFileName from '@/components/generate/TitleAndFileName'
+import { formatVideoURL } from '@/utils/urlFormater'
+import { api } from '@/lib/api'
 
 const Generate = () => {
+  const [videoUrl, setVideoUrl] = useState('')
+
+  async function handleVideoURL(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const inputUrl = String(formData.get('url'))
+
+    const { embedURL, videoID } = formatVideoURL(inputUrl)
+    setVideoUrl(embedURL)
+
+    await api.get('/audio', {
+      params: {
+        videoID: videoID
+      }
+    })
+  }
   return (
     <div className='flex flex-col w-full gap-5'>
       <TitleAndFileName />
 
-      <form className='flex gap-3'>
+      <form onSubmit={handleVideoURL} className='flex gap-3'>
         <Input
           required
           placeholder='Insira a URL do vídeo do youtube...'
@@ -19,13 +38,20 @@ const Generate = () => {
         />
 
         <Button type='submit'>
-          Enviar
+          {videoUrl ? 'Enviado' : 'Enviar'}
           <CheckCheckIcon size={15} />
         </Button>
       </form>
 
       <div className='border border-zinc-600 p-4 rounded-md flex flex-col gap-3'>
-        <EmptyVideo />
+        {
+          videoUrl ? (
+            <iframe src={videoUrl} className='aspect-video h-64 rounded-md'>
+
+            </iframe>
+          ) : <EmptyVideo />
+        }
+
         <PromptForm />
       </div>
     </div>
