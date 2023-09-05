62611862898d65bf943165b8f05d07314f645c27
Matheus Motta <matheusdomingues423@gmail.com>
2023-09-05 16:15:36 -0300

    openai-config

--- server/lib/openai.ts
+++ server/lib/openai.ts
@@ -1,3 +1,5 @@
 import OpenAI from "openai";
 
-export const openai = new OpenAI({ apiKey: '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' });
+export const openai = new OpenAI({
+  apiKey: process.env["OPENAI_API_KEY"]
+});
